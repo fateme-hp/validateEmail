24e8d4921f2eb82a1d7b10e90eed8314618b036e
fateme-hp <fatxnstrn@gmail.com>
2022-07-15 19:42:30 +0430

    comments

--- script.js
+++ script.js
@@ -18,8 +18,6 @@ sender.addEventListener("blur", validateForm);
 receiver.addEventListener("blur", validateForm);
 emailSubject.addEventListener("blur", validateForm);
 emailContent.addEventListener("blur", validateForm);
-// emailSubject.addEventListener("blur", validateEmail);
-// emailContent.addEventListener("blur", validateEmail);
 resetBtn.addEventListener("click", resetStyle);
 
 // function
@@ -33,6 +31,8 @@ function appInitial() {
 function validateForm() {
   validateField(this);
 
+  // check the input type 
+  // if input type is email , do the email validation 
   if (this.type === "email") {
     validateEmail(this);
   }
@@ -42,11 +42,16 @@ function validateForm() {
 
 // check if input has value
 function validateField(field) {
+  // if input has no value 
+  // change border color to red and remove correct class
   if (!field.value) {
     field.style.borderBottomColor = "#f36656d7";
     field.style.borderBottomWidth = "2px";
     field.classList.remove("correct");
-  } else {
+  } 
+   // if input has value 
+  // change border color and add correct class
+  else { 
     field.style.borderBottomColor = "#00b4b7bb";
     field.style.borderBottomWidth = "2px";
     field.classList.add("correct");
@@ -56,13 +61,18 @@ function validateField(field) {
 // validate email
 
 function validateEmail(field) {
+  // get value of email input 
   const emailText = field.value;
 
+  // check if it contains "@" 
+  // if email value includes "@"
+  // change border color and add correct class
   if (emailText.includes("@")) {
     field.style.borderBottomColor = "#00b4b7bb";
     field.style.borderBottomWidth = "2px";
     field.classList.add("correct");
-  } else {
+  }  // otherwise change border color to red and remove correct class
+  else {
     field.style.borderBottomColor = "#f36656d7";
     field.style.borderBottomWidth = "2px";
     field.classList.remove("correct");
@@ -72,8 +82,11 @@ function validateEmail(field) {
 // after filling required fields, active send button
 
 function activeBtn() {
+  // create a nodeList of correct class 
   const correct = document.querySelectorAll(".correct");
 
+  // check the length of nodeList 
+  // if length is 4 active the send button 
   if (correct.length === 4) {
     sendBtn.disabled = false;
   } else {
@@ -81,8 +94,22 @@ function activeBtn() {
   }
 }
 
+// if reset button was clicked 
 function resetStyle(field) {
-   location.reload();
-  // console.log(field);
-  // field.borderBottomColor = "none";
+
+  // refresh the page 
+  //  location.reload();
+ 
+//or 
+
+// change inputs border bottom 
+
+// create a nodeList of input elements 
+  const borderedInput = document.querySelectorAll("form input , form textarea");
+// for each input change border style to default design 
+  borderedInput.forEach(element => {
+    element.style.borderBottomColor = "#fff";
+    element.style.borderBottomWidth = "1px";
+  });
+  
 }
